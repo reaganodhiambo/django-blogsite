5d8f853949edd9d2dfc383e533801b458c1b9e9a
reaganodhiambo <reaganodhiambo003@gmail.com>
2022-05-13 10:15:33 +0300

    deletes

--- src/shoptask/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    #path('<slug:slug>/', views.PostDetail.as_view(),name='post_detail'),
-    path('blog/<slug:slug>/', views.postDetail,name='post_detail'),
-    path('register/', views.registerPage, name='register')
-]
